826899601adc742d5a3f2295f8a671be98b902ab
GopalPatoliya <grpatoliya2110@gmail.com>
2025-05-02 17:02:57 +0530

    case of newline as delimiter

--- spec/lib/string_calculator_spec.rb
+++ spec/lib/string_calculator_spec.rb
@@ -18,5 +18,9 @@ RSpec.describe StringCalculator do
     it 'handles multiple numbers' do
       expect(described_class.add("1,2,3,4")).to eq(10)
     end
+
+    it 'supports newlines as delimiter' do
+      expect(described_class.add("1\n2,3")).to eq(6)
+    end
   end
 end
