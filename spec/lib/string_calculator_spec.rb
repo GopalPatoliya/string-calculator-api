e15fc7c8c7a9d63411147c770757b00a5f0ec90b
GopalPatoliya <grpatoliya2110@gmail.com>
2025-05-02 16:58:35 +0530

    case of single number returns itself

--- spec/lib/string_calculator_spec.rb
+++ spec/lib/string_calculator_spec.rb
@@ -6,5 +6,9 @@ RSpec.describe StringCalculator do
     it 'returns 0 for an empty string' do
       expect(described_class.add("")).to eq(0)
     end
+
+    it 'returns number for single number' do
+      expect(described_class.add("5")).to eq(5)
+    end
   end
 end
