3dfa64a4e4617795dfd39fc38009b3d434ea1d34
GopalPatoliya <grpatoliya2110@gmail.com>
2025-05-02 17:18:38 +0530

    Raise exception for negative numbers

--- spec/lib/string_calculator_spec.rb
+++ spec/lib/string_calculator_spec.rb
@@ -26,5 +26,11 @@ RSpec.describe StringCalculator do
     it 'supports custom single-character delimiter' do
       expect(described_class.add("//;\n1;2")).to eq(3)
     end
+
+    it 'raises error for negative numbers' do
+      expect {
+        described_class.add("1,-2,3,-5")
+      }.to raise_error("negative numbers not allowed -2,-5")
+    end
   end
 end
